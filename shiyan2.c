d11e36c73a9d08595e1594a195160f2efd705602
yuanbin-wu <17346682428@163.com>
2019-10-12 18:20:38 +0800

    commit shiyan 20191012

---
+++ shiyan2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include"twj.h"
+int main(int argc, char **argv)
+{
+
+	
+	int count;
+	float beita[N];
+	stuData s[N];
+	count=input(s);
+	Beita(s,beita,count);
+	print(beita,count);
+	return 0;
+}
